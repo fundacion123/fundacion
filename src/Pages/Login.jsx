cd8436e0bbd4ba5f153b15ad7c405fc62ccbff40
pepocasver <147658811+pepocasver@users.noreply.github.com>
2024-04-08 18:50:31 -0600

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -42,7 +42,7 @@ function Login() {
       <form className='form' onSubmit={handleSubmit}>
         <input className='input' placeholder='Your email' type='email' name='email' onChange={handleChange} value={form.email} />
         <input className='input' placeholder='Your password' type='password' name='password' onChange={handleChange} value={form.password} />
-        <button className='form-button'>Log In.</button>
+        <button className='form-button'>Log In</button>
         {/* Botón adicional "Go to Dapp" */}
         <a href="https://pepocasver.github.io/IEBS_Refutoken_Dapp/" target="_blank" rel="noopener noreferrer" className='dapp-button'>Go to Dapp</a>
         <Link to="/register">Not registered? Click here</Link>
